3fe6a807817220ef9bf1d6c84c8800b8b6720b5e
e1mostafa <elmostafaabdelaal@gmail.com>
2026-06-04 23:03:34 +0300

    fix typescript errors

--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -10,6 +10,7 @@ export const logger = winston.createLogger({
     json()
   ),
   transports: [
+    new winston.transports.Console(),
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
